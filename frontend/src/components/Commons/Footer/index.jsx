3c6ddd385d12bfa91f1a1290a9850933581ecb05
Miran <miranfirdausi027@gmail.com>
2024-07-28 17:46:46 +0530

    add landing page

---
+++ frontend/src/components/Commons/Footer/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p>&copy; 2024 Clip Kadabra. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
